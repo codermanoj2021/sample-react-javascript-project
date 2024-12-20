473e797cda9b055fcc3eeea6ba50444b7aad70be
Manoj Singh Rawat <natkhatkanharawatji@gmail.com>
2024-12-20 16:21:56 +0530

    creating sample react javascript project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-96 w-96 bg-black text-white' > 
+      jai shree ram
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust paths based on your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
